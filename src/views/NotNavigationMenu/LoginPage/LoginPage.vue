f6032ae00ef8f6b6d1ebae7f5e515b96e9829393
liliuyi <1440392609@qq.com>
2024-05-22 15:05:27 +0800

    增加登录背景视频

--- src/views/NotNavigationMenu/LoginPage/LoginPage.vue
+++ src/views/NotNavigationMenu/LoginPage/LoginPage.vue
@@ -1,31 +1,66 @@
 <template>
 	<div class="LoginPage whv100">
-		<div class="login-region">
-			<div class="enter-region">
-				<!--<template v-for="(item,index) in "></template>-->
-				<!--<a-input v-model:value="value" placeholder="Basic usage" />-->
-				<!--<a-input v-model:value.lazy="value1" autofocus placeholder="Lazy usage" />-->
+		<div class="background-video wh100 dfc">
+			<!--<video class="wh100 dfc" muted autoplay src="@/assets/PublicVideos/keqingzatan.mp4"></video>-->
+			<video class="wh100 dfc" muted autoplay src=""></video>
+		</div>
+		<div class="login-region wh100 dfc">
+			<div class="region-box w-500 p-24 bor-20">
+				<div class="enter-region">
+					<template v-for="(item,index) in allData['enterData']" :key="index">
+						<div class="enter-box df flr jc-fs alc">
+							<div class="title df flr jc-fe alc w-100">
+								<p>{{ item.title }}：</p>
+							</div>
+							<a-input v-model:value="item.value" allowClear :placeholder="item['pla']" v-if="item['type'] === 'input'" />
+							<a-input-password  v-model:value="item.value" allowClear :placeholder="item['pla']" v-else-if="item['type'] === 'pass'" />
+						</div>
+					</template>
+				</div>
+				<a-button type="primary" class="w100 h-40 m-t-20 fs-20" @click="LoginCon">登录</a-button>
 			</div>
-			<div class="btn-region"></div>
 		</div>
 	</div>
 </template>
 
 <script setup name="LoginPage">
 import LoginPage from './config/LoginPage.json'
-import {reactive} from "vue";
+import {reactive, ref} from "vue";
 import {deepCopy} from "@/utils/GlobalApproach.js";
-import pinia from '@/pinia'
 
 const allData = reactive(deepCopy(LoginPage))
-console.log(pinia.userInfoStore.userBasicInfo)
-console.log(import.meta.env)
+
+// 登录内容
+async function LoginCon() {
+
+}
+
+
 </script>
 
 <style scoped lang="scss">
 .LoginPage {
-	background-image: url("@/assets/PublicImages/keqing1.jpg");
-	background-repeat: no-repeat;
-	background-size: 100% 100%;
+	.background-video {
+		background-color: #000000;
+		video {
+			object-fit: fill;
+		}
+	}
+	.login-region {
+		position: fixed;
+		top: 0;
+		left: 0;
+		.region-box {
+			background-color: #ffffff;
+			.enter-region {
+				.enter-box {
+					margin-bottom: 20px;
+				}
+				.enter-box:last-child {
+					margin-bottom: 0;
+				}
+			}
+		}
+	}
 }
 </style>
